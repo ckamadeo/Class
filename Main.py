c0980a286b1c122bb63b8e9344ac9ea5e1d10128
Claude Amadeo <claudeamadeo@Charizard.local>
2018-02-09 13:56:01 -0500

    completed reorganization

---
+++ Main.py
@@ -0,0 +1,23 @@
+# import helper libraries
+import sys
+import StringProcessor as sp
+
+# Get input from the command line
+# argv[0] is the name of the script, first input starts at argv[1]
+str_input = sys.argv[1]
+
+
+# main routine
+
+# todo: preprocess string to remove punctuation
+# todo: preprocess string to normalize casing
+
+TotalWords = sp.TotalWords(str_input)
+PhraseLength = sp.PhraseLength(str_input)
+WordCount = sp.WordCount(str_input)
+
+print "Total Words: %d" % TotalWords
+print "Phrase Length: %d" % PhraseLength
+
+# todo: format output better
+print WordCount
