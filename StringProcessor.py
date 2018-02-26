85efc4759b5205c1fd5a477340283dd8b6336210
Claude Amadeo <claudeamadeo@Charizard.local>
2018-02-26 12:44:19 -0500

    implemented and tested

--- StringProcessor.py
+++ StringProcessor.py
@@ -1,11 +1,28 @@
+import string
+
+
+# make strings more uniform for comparison
+def PrepString(str_input):
+
+    # brute force replacement
+    # there are more optimized ways of doing this
+    for character in string.punctuation:
+        str_input = str_input.replace(character,"")
+
+    # convert everything to lower case to make it case insensitive
+    str_input = str_input.lower()
+
+    return str_input
+
+
 # returns the total number of words in an input phrase
 def TotalWords(str_input):
-    words = str_input.split()
+    words = PrepString(str_input).split()
     return len(words)
 
 # returns the total number of characters in an input phrase
 def PhraseLength(str_input):
-    return len(str_input)
+    return len(PrepString(str_input))
 
 # returns an array of words and their count in a phrase
 def WordCount(str_input):
@@ -13,7 +30,7 @@ def WordCount(str_input):
     # initialize dictionary of results
     dict_WordSet = {}
 
-    words = str_input.split()
+    words = PrepString(str_input).split()
     for word in words:
         if word in dict_WordSet:
             dict_WordSet[word] += 1
@@ -22,3 +39,17 @@ def WordCount(str_input):
 
     # todo: order results from largest to smallest values
     return dict_WordSet
+
+# returns a list of the distinct words in a phrase
+def DistinctWords(str_input):
+
+    dict_WordSet = WordCount(str_input)
+
+    return dict_WordSet.keys()
+
+# returns the count of the distinct number of words in a phrase
+def DistinctWordCount(str_input):
+
+    dict_WordSet = WordCount(str_input)
+
+    return len(dict_WordSet.keys())
